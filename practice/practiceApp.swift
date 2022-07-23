feb18bcbba5ab85ae5440c983417b32d7171c365
dwson <realdongwooson@gmail.com>
2022-07-23 21:44:35 +0900

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Dongwoo Son on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
